d35b5daf790dbdfb8686d82bddba5b446a869058
Chris Gyurgyik <37983775+cgyurgyik@users.noreply.github.com>
2019-11-24 18:15:56 -0500

    Create bin_search.s

---
+++ bin_search.s
@@ -0,0 +1,93 @@
+# Binary search
+#
+# bin_search(int[] arr, int l, int r, int x)
+# int[] arr: memory address of 'arr'. Assumes each element is size 4.
+# l: left boundary.
+# r: right boundary.
+# x: element being searched for.
+# 
+#
+# Requires: r >= l. Returns -1 otherwise.
+#
+# Limitations: Uses the Cornell RISC-V Interpreter which has limited instruction set, so some instructions are
+#              implemented using other instructions (i.e. MUL).
+
+# MAIN
+addi sp, sp, 10000
+addi a0, x0, 0 # hardcode the memory address to begin at 0.
+addi a1, x0, 0 # hardcode l = 1.
+addi a2, x0, 4 # hardcore r = 5.
+addi a3, x0, 2 # hardcode x = 5.
+
+# Store array values in contiguous memory:
+# {1, 3, 5, 10, 27, 45}
+ addi t0, x0, 1
+ sw t0, 0(a0) 
+ addi t0, x0, 3
+ sw t0, 4(a0)
+ addi t0, x0, 5
+ sw t0, 8(a0)
+ addi t0, x0, 10
+ sw t0, 12(a0)
+ addi t0, x0, 27
+ sw t0, 16(a0)
+ addi t0, x0, 45
+ sw t0, 20(a0)
+
+jal ra, BIN_SEARCH
+jal ra, EXIT
+
+BIN_SEARCH:
+addi sp, sp, 8
+sw ra, 4(sp)
+sw s0, 0(sp)
+
+blt a2, a1, INCORRECT_BOUNDS # if (r < l) return -1.
+
+
+sub t0, a2, a1 # store r-l.
+srai t1, t0, 1 # store (r-l)/2.
+add t2, t1, a0 # store l + (r-l)/2.
+
+# Get the 'mid' element in 'arr'.
+addi s0, x0, 0 # clear s0.
+addi t0, x0, 0 # clear t0.
+
+LOOP:
+beq t0, t2, END_LOOP
+addi t0, t0, 1
+addi s0, s0, 4  # mid * sizeof(int)
+beq x0, x0, LOOP
+END_LOOP:
+
+add s0, s0, a0 # pointer to arr[mid].
+lw t0, 0(s0)   # get value at arr[mid].
+
+bne t0, a3, SKIP_ONE # if (arr[mid] == x) return mid.
+add a0, x0, t2
+beq x0, x0, RET
+
+SKIP_ONE:
+blt t0, a3, SKIP_TWO # if (arr[mid] > x) return bin_search(arr, l, mid-1, x).
+addi a2, t2, -1 # r = mid - 1
+jal ra, BIN_SEARCH
+
+SKIP_TWO:
+addi a1, t2, 1 # l = mid + 1
+jal ra, BIN_SEARCH
+
+RET: 
+lw s0, 0(sp)
+lw ra, 4(sp)
+addi sp, sp, 8
+jalr x0, ra, 0
+
+INCORRECT_BOUNDS:
+addi a0, x0, -1
+lw s0, 0(sp)
+lw ra, 4(sp)
+addi sp, sp, 8
+jalr x0, ra, 0
+
+EXIT:
+

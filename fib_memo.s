284ed73b20ce575c81352f810917f0953a93e1aa
Chris Gyurgyik <37983775+cgyurgyik@users.noreply.github.com>
2019-11-24 20:02:10 -0500

    Update documentation.

--- fib_memo.s
+++ fib_memo.s
@@ -1,92 +1,74 @@
-# Binary search
+# Memoized version of Fibonacci sequence.
 #
-# bin_search(int[] arr, int l, int r, int x)
-# int[] arr: memory address of 'arr'. Assumes each element is size 4.
-# l: left boundary.
-# r: right boundary.
-# x: element being searched for.
-# 
-#
-# Requires: r >= l. Returns -1 otherwise.
+# fib_memo(n, arr, size)
+# n: nth fibonacci number.
+# arr: memory address of the array. Assumes each element is size 4 (int).
+# size: size of the array.
 #
 # Limitations: Uses the Cornell RISC-V Interpreter which has limited instruction set, so some instructions are
 #              implemented using other instructions (i.e. MUL).
 
+
 # MAIN
 addi sp, sp, 10000
-addi a0, x0, 0 # hardcode the memory address to begin at 0.
-addi a1, x0, 0 # hardcode l = 0.
-addi a2, x0, 4 # hardcore r = 6.
-addi a3, x0, 10 # hardcode x = 10.
-
-# Store array values in contiguous memory:
-# {2, 3, 4, 10, 40, 50, 1000}
- addi t0, x0, 2
- sw t0, 0(a0) 
- addi t0, x0, 3
- sw t0, 4(a0)
- addi t0, x0, 4
- sw t0, 8(a0)
- addi t0, x0, 10
- sw t0, 12(a0)
- addi t0, x0, 40
- sw t0, 16(a0)
- addi t0, x0, 50
- sw t0, 20(a0)
- addi t0, x0, 1000
- sw t0, 24(a0)
-
-jal ra, BIN_SEARCH
-
-BIN_SEARCH:
-addi sp, sp, 8
-sw ra, 4(sp)
-sw s0, 0(sp)
-
-blt a2, a1, INCORRECT_BOUNDS # if (r < l) return -1.
-
-sub t0, a2, a1 # store r-l.
-srai t1, t0, 1 # store (r-l)/2.
-add t2, t1, a1 # store l + (r-l)/2.
-
-# Get the 'mid' element in 'arr'.
-addi s0, x0, 0 # clear s0.
-addi t0, x0, 0 # clear t0.
-
-LOOP:
-beq t0, t2, END_LOOP
-addi t0, t0, 1
-addi s0, s0, 4  # mid * sizeof(int)
-beq x0, x0, LOOP
-END_LOOP:
-
-add s0, s0, a0 # pointer to arr[mid].
-lw t0, 0(s0)   # get value at arr[mid].
-
-bne t0, a3, SKIP_ONE # if (arr[mid] == x) return mid.
-add a0, x0, t2
-beq x0, x0, RET
-
-SKIP_ONE:
-blt t0, a3, SKIP_TWO # if (arr[mid] > x) return bin_search(arr, l, mid-1, x).
-addi a2, t2, -1 # l = mid - 1
-jal ra, BIN_SEARCH
-
-SKIP_TWO:
-addi a1, t2, 1 # r = mid + 1
-jal ra, BIN_SEARCH
-
-RET: 
-lw s0, 0(sp)
-lw ra, 4(sp)
-addi sp, sp, 8
+addi a0, x0, 10    # hardcode n = 10
+addi a1, x0, 0     # hardcode memory address of 'arr'.
+addi a2, x0, 100   # hardcode size of 'arr'.
+jal ra, FIB_MEMO
 jal ra, EXIT
 
-INCORRECT_BOUNDS:
-addi a0, x0, -1
+FIB_MEMO:
+addi sp, sp, -16
+sw ra, 12(sp)
+sw s2, 8(sp)
+sw s1, 4(sp)
+sw s0, 0(sp)
+
+addi s0, a0, 0     # save n.
+addi s2, x0, 0     # clean slot for s2.
+
+# multiply n * 4 without using MUL.
+addi t0, x0, 0   
+LOOP:
+BEQ t0, s0, LOOP_DONE
+addi t0, t0, 1     # i = i + 1
+addi s2, s2, 4     # memory address = memory address + sizeof(int)
+BEQ x0, x0, LOOP
+LOOP_DONE:
+
+add s2, s2, a1     # arr[n]
+
+BLT a2, a0, SKIP   # if (size < n) skip. 
+
+lw a0, 0(s2)       # load value at arr[n].
+BNE a0, x0, RET    # if arr[n] != 0 return.
+
+SKIP:
+addi a0, s0, 0     # get value of n again.
+beq a0, x0, RET    # if n == 0 return 0;
+
+addi t1, x0, 1
+beq a0, t1, RET    # if n == 1 return 1;
+
+addi a0, a0, -2    # n-2
+jal ra, FIB_MEMO   # fib_memo(n-2, ...)
+
+addi s1, a0, 0     # store return value of fib_memo
+addi a0, s0, 0     # get value of n again.
+addi a0, a0, -1    # n-1
+jal ra, FIB_MEMO   # fib_memo(n-1, arr, size)
+
+add a0, a0, s1     # fib_memo(n-1, arr, size) + fib_memo(n-2, arr, size)
+
+BLT a2, s0, RET    # if (size < n) skip.
+sw a0, 0(s2)       # store the answer at arr[n].
+
+RET:
 lw s0, 0(sp)
-lw ra, 4(sp)
-addi sp, sp, 8
+lw s1, 4(sp)
+lw s2, 8(sp)
+lw ra, 12(sp)
+addi sp, sp, 16
 jalr x0, ra, 0
 
 EXIT:

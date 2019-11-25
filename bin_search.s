cd2ea75b5f483df73de44c0cdcba5512d2939c4a
Chris Gyurgyik <37983775+cgyurgyik@users.noreply.github.com>
2019-11-24 20:01:00 -0500

    Fixed bug.

--- bin_search.s
+++ bin_search.s
@@ -15,24 +15,26 @@
 # MAIN
 addi sp, sp, 10000
 addi a0, x0, 0 # hardcode the memory address to begin at 0.
-addi a1, x0, 0 # hardcode l = 1.
-addi a2, x0, 4 # hardcore r = 5.
-addi a3, x0, 2 # hardcode x = 5.
+addi a1, x0, 0 # hardcode l = 0.
+addi a2, x0, 4 # hardcore r = 6.
+addi a3, x0, 10 # hardcode x = 10.
 
 # Store array values in contiguous memory:
-# {1, 3, 5, 10, 27, 45}
- addi t0, x0, 1
+# {2, 3, 4, 10, 40, 50, 1000}
+ addi t0, x0, 2
  sw t0, 0(a0) 
  addi t0, x0, 3
  sw t0, 4(a0)
- addi t0, x0, 5
+ addi t0, x0, 4
  sw t0, 8(a0)
  addi t0, x0, 10
  sw t0, 12(a0)
- addi t0, x0, 27
+ addi t0, x0, 40
  sw t0, 16(a0)
- addi t0, x0, 45
+ addi t0, x0, 50
  sw t0, 20(a0)
+ addi t0, x0, 1000
+ sw t0, 24(a0)
 
 jal ra, BIN_SEARCH
 jal ra, EXIT
@@ -44,10 +46,9 @@ sw s0, 0(sp)
 
 blt a2, a1, INCORRECT_BOUNDS # if (r < l) return -1.
 
-
 sub t0, a2, a1 # store r-l.
 srai t1, t0, 1 # store (r-l)/2.
-add t2, t1, a0 # store l + (r-l)/2.
+add t2, t1, a1 # store l + (r-l)/2.
 
 # Get the 'mid' element in 'arr'.
 addi s0, x0, 0 # clear s0.
@@ -69,18 +70,18 @@ beq x0, x0, RET
 
 SKIP_ONE:
 blt t0, a3, SKIP_TWO # if (arr[mid] > x) return bin_search(arr, l, mid-1, x).
-addi a2, t2, -1 # r = mid - 1
+addi a2, t2, -1 # l = mid - 1
 jal ra, BIN_SEARCH
 
 SKIP_TWO:
-addi a1, t2, 1 # l = mid + 1
+addi a1, t2, 1 # r = mid + 1
 jal ra, BIN_SEARCH
 
 RET: 
 lw s0, 0(sp)
 lw ra, 4(sp)
 addi sp, sp, 8
-jalr x0, ra, 0
+jal ra, EXIT
 
 INCORRECT_BOUNDS:
 addi a0, x0, -1
